c637b4d93470db5b12179ebe90a8b2973e6be24f
Thioro <fatoumata-thioro.bah@capgemini.com>
2026-03-09 17:08:27 +0100

    Ajout logique de jeu: mot masque, clavier et gestion des erreurs

---
+++ lib/data/word_list.dart
@@ -0,0 +1,18 @@
+// Liste des mots pour le jeu du pendu
+final List<String> wordList = [
+  'FLUTTER',
+  'DART',
+  'WIDGET',
+  'ANDROID',
+  'MOBILE',
+  'CLAVIER',
+  'PENDU',
+  'MIAGE',
+  'LOGICIEL',
+  'PROJET',
+  'ECRAN',
+  'BOUTON',
+  'CLASSE',
+  'METHODE',
+  'VARIABLE',
+];
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,23 +1,96 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:hangman_game/data/word_list.dart';
 
-class GameScreen extends StatefulWidget{
-  const GameScreen ({super.key});
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
 
   @override
   State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  late String _wordToGuess;
+  Set<String> _guessedLetters = {};
+  int _errors = 0;
+  final int _maxErrors = 6;
+
+  @override
+  void initState() {
+    super.initState();
+    _startNewGame();
   }
 
-  class _GameScreenState extends State<GameScreen>{
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Jeu du Pendu'),
-          backgroundColor: Colors.deepPurple,
-        ),
-        body: Center(
-          child: Text('Ici on va jouer!'),
-        ),
-      );
-    }
+  void _startNewGame() {
+    final random = Random();
+    setState(() {
+      _wordToGuess = wordList[random.nextInt(wordList.length)];
+      _guessedLetters = {};
+      _errors = 0;
+    });
   }
+
+  String _getDisplayedWord() {
+    return _wordToGuess
+        .split('')
+        .map((letter) => _guessedLetters.contains(letter) ? letter : '_')
+        .join(' ');
+  }
+
+  void _guessLetter(String letter) {
+    setState(() {
+      _guessedLetters.add(letter);
+      if (!_wordToGuess.contains(letter)) {
+        _errors++;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jeu du Pendu'),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //nombre d'erreurs
+          Text(
+            'Erreurs: $_errors / $_maxErrors',
+            style: TextStyle(fontSize: 20, color: Colors.red),
+          ),
+          SizedBox(height: 30),
+          // mot masqué
+          Text(
+            _getDisplayedWord(),
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 5,
+            ),
+          ),
+          SizedBox(height: 40),
+          //clavier
+          Wrap(
+            alignment: WrapAlignment.center,
+            spacing: 5,
+            runSpacing: 5,
+            children: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) {
+              final isGuessed = _guessedLetters.contains(letter);
+              return ElevatedButton(
+                onPressed: isGuessed ? null : () => _guessLetter(letter),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: isGuessed ? Colors.grey : Colors.deepPurple,
+                  minimumSize: Size(40, 40),
+                ),
+                child: Text(letter, style: TextStyle(color: Colors.white)),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
